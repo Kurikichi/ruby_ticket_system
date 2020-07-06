b7008f0ec2117f1ffd78cce943e85235a1966fcd
Kurikichi <katasuke1215@gmail.com>
2020-07-06 16:52:37 +0900

    Update ticket_vending_machine.rb

--- ticket_vending_machine.rb
+++ ticket_vending_machine.rb
@@ -3,7 +3,21 @@ class TicketVendingMachine
     @movies = []
   end
 
+  def movies
+    @movies
+  end
+
   def movies=(movies)
     @movies = movies
+  end
+
+  def display_menu
+    puts "どの映画を見ますか？"
+    i = 0
+    self.movies.each do |movie|
+      puts "#{i} #{movie.title}: #{movie.fee}円"
+      i += 1
+    end
+    gets
   end
 end
